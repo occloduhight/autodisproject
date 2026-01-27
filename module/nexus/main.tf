11c7431e9822e001669578b122f7be63bb4c94f8
occloduhight <chinweodochi@gmail.com>
2026-01-27 15:15:11 +0100

    modified nexus main.tf

--- module/nexus/main.tf
+++ module/nexus/main.tf
@@ -76,17 +76,23 @@ resource "aws_security_group" "nexus_elb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 resource "aws_security_group" "nexus_sg" {
   name        = "${var.name}-nexus-sg"
-  description = "Allow ELB to Nexus"
+  description = "Allow access to Nexus"
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 8081
-    to_port         = 8081
-    protocol        = "tcp"
-    security_groups = [aws_security_group.nexus_elb_sg.id]
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -96,6 +102,26 @@ resource "aws_security_group" "nexus_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# resource "aws_security_group" "nexus_sg" {
+#   name        = "${var.name}-nexus-sg"
+#   description = "Allow ELB to Nexus"
+#   vpc_id      = var.vpc_id
+
+#   ingress {
+#     from_port       = 8081
+#     to_port         = 8081
+#     protocol        = "tcp"
+#     security_groups = [aws_security_group.nexus_elb_sg.id]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 # Classic ELB for Nexus
 resource "aws_elb" "nexus_elb" {
