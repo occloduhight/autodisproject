a4aaa7a25e0e2fed8f88b0bf3082436d0c2bf2c1
occloduhight <chinweodochi@gmail.com>
2026-03-09 22:28:36 +0100

    update VPC module

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -28,7 +28,7 @@ resource "aws_subnet" "pub-sub1" {
 
 resource "aws_subnet" "pub-sub2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = true
 
